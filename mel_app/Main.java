7e37818901575cf0498d1b6ac24952d1a77c93ce
Lando <landoicco@gmail.com>
2023-11-26 14:38:17 -0600

    Basic JSON handling working  - Cats and dogs JSON objects correctly handled in code

--- mel_app/Main.java
+++ mel_app/Main.java
@@ -1,50 +1,10 @@
-import animals.Cat;
-import database.CatJDBC;
-import helpers.NewAnimal;
 import helpers.JsonFileReader;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 public class Main {
     public static void main(String args[]) {
         System.out.println("== Animals Management app ==");
-        // Animal testAnimal = NewAnimal.createNewCat();
-        // System.out.println(testAnimal);
-        Cat testAnimal = new Cat(001, "Max", "male", "22-22-2222", true, null, 
-            null, null, null, null);
-        Cat testAnimal2 = new Cat(001, "Pistacha", "female", "22-22-2222", true, null, 
-        null, null, null, null);
 
-        // In java JSONObject is used to create JSON object
-        // which is a subclass of java.util.HashMap.
- 
-        // JSONObject file = new JSONObject();
- 
-        // file.put("Full Name", "Ritu Sharma");
-        // file.put("Roll No.", new Integer(1704310046));
-        // file.put("Tuition Fees", new Double(65400));
- 
-        // // To print in JSON format.
-        // System.out.print(file);
-
-         // Converting JSON data into Java String format
-          String k = "{\"Full Name\":\"Lando Icaza\", \"Tuition Fees\":65400.0, \"Roll No.\":1704310046}";
-          Object file = JSONValue.parse(k);
-   
-          // In java JSONObject is used to create JSON object
-          JSONObject jsonObjectdecode = (JSONObject)file;
-   
-          // Converting into Java Data type
-          // format From Json is the step of Decoding.
-          String name
-              = (String)jsonObjectdecode.get("Full Name");
-          double fees
-              = (Double)jsonObjectdecode.get("Tuition Fees");
-          long rollno
-              = (Long)jsonObjectdecode.get("Roll No.");
-          System.out.println(name + " " + fees + " "
-                             + rollno);
-
-        JsonFileReader.printJsonData();
+        // Tests on JsonFileReader
+        System.out.println(JsonFileReader.getAnimalsMap().get("dogs"));
     }
 }
--- mel_app/helpers/JsonFileReader.java
+++ mel_app/helpers/JsonFileReader.java
@@ -1,20 +1,61 @@
 package helpers;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JsonFileReader {
 
+    // Path where the json file lives
+    private static Path path = Paths.get("lib/data.json");
+    private static Map<String, JSONArray> animals = new HashMap<>();
+
     // Print to console all the contents of the file
-    public static void printJsonData() {
-        Path path = Paths.get("lib/data.json");
+    public static void printAllJsonData() {
         try(var s = Files.lines(path)) {
             s.forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static Map<String, JSONArray> getAnimalsMap() {
+
+        JSONParser jsonParser = new JSONParser();
+        StringBuilder sb = new StringBuilder();
+
+        try (var s = Files.lines(path)) {
+            s.forEach(sb::append);
+
+            Object fullJsonObject = jsonParser.parse(sb.toString());
+            JSONArray fullArray = (JSONArray) fullJsonObject;
+
+            fullArray.forEach(type -> createAnimalsMap((JSONObject) type));
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return animals;
+    }
+
+    private static void createAnimalsMap(JSONObject obj) {
+        JSONArray c = (JSONArray) obj.get("cats");
+        JSONArray d = (JSONArray) obj.get("dogs");
+
+        if (c != null) {
+            animals.put("cats", c);
+        } else {
+            animals.put("dogs", d);
         }
     }
 
